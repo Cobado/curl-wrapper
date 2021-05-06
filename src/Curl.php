60ba5f2fda73e4a9005fdea190ca37badd6c1022
Nebado <ruslan.nbd@gmail.com>
2021-05-06 23:41:09 +0300

    Add put and delete method

--- src/Curl.php
+++ src/Curl.php
@@ -1,6 +1,8 @@
 <?php
 
 class Curl {
+    const DEFAULT_USERPWD = 'test: test@domain.com';
+    
     /** @var bool */
     public $proxy = false;
     
@@ -85,6 +87,58 @@ class Curl {
         $curlOptions['CURLOPT_POST'] = 1;
         $curlOptions['CURLOPT_POSTFIELDS'] = $params;
         return $this->request($url, $curlOptions);
+    }
+
+    /**
+     * HTTP PUT method
+     *
+     * @param string $url
+     * @param array $params
+     * @param array $curlOptions
+     * @return bool
+     */
+    public function put($url, $params = array(), $curlOptions = array()) {
+        $file = $params['file'];
+        
+        if (!is_file($file)){
+            return null;
+        }
+        
+        $fp   = fopen($file, 'r');
+        $size = filesize($file);
+        
+        $curlOptions['CURLOPT_PUT']        = 1;
+        $curlOptions['CURLOPT_INFILESIZE'] = $size;
+        $curlOptions['CURLOPT_INFILE']     = $fp;
+        
+        if (!isset($this->curlOptions['CURLOPT_USERPWD'])) {
+            $curlOptions['CURLOPT_USERPWD'] = self::DEFAULT_USERPWD;
+        }
+        
+        $ret = $this->request($url, $curlOptions);        
+        fclose($fp);
+        
+        return $ret;
+    }
+
+    /**
+     * HTTP DELETE method
+     *
+     * @param string $url
+     * @param array $params
+     * @param array $curlOptions
+     * @return bool
+     */
+    public function delete($url, $param = array(), $curlOptions = array()) {        
+        $curlOptions['CURLOPT_CUSTOMREQUEST'] = 'DELETE';
+        
+        if (!isset($curlOptions['CURLOPT_USERPWD'])) {
+            $curlOptions['CURLOPT_USERPWD'] = self::DEFAULT_USERPWD;
+        }
+        
+        $ret = $this->request($url, $curlOptions);
+        
+        return $ret;
     }
 }
 
